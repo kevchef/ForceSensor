a8cae258064b7956d8eb84be1dd6ef155d2ec96e
Kevin Schneider <skevin@student.ethz.ch>
2019-06-04 16:04:21 +0200

    ForceAnimationCircleTest

---
+++ ForceSensor/ForceAnimationCircle.swift
@@ -0,0 +1,21 @@
+//
+//  ForceAnimationCircle.swift
+//  ForceSensor
+//
+//  Created by Kevin Schneider on 04.06.19.
+//  Copyright © 2019 Kevin Schneider. All rights reserved.
+//
+
+import UIKit
+
+class ForceAnimationCircle: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
